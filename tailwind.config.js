c12cfd6124e9ebf6161489ca298be02f8d3e1d7b
Kadir Emre Parlak <kemreparlak@gmail.com>
2023-04-09 06:34:18 +0300

    First day codes..

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#0095f6",
+      },
+      backgroundImage: {
+        "login-bg":
+          "url(https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png)",
+      },
+    },
+  },
+  plugins: [],
+};
