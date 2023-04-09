20b84edb464b71e82cb9024068a38e2655151bf3
Kadir Emre Parlak <kemreparlak@gmail.com>
2023-04-09 18:36:29 +0300

    redux configuration added. auth station controlled

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import AuthLayout from "./auth";
+import HomePage from "./pages/HomePage";
+import PrivateRoute from "./components/PrivateRoute";
+import LoginPage from "./pages/LoginPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <HomePage />,
+    auth: true,
+  },
+  {
+    path: "auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+];
+const authCheck = (routes) =>
+  routes.map((route) => {
+    if (route?.auth) {
+      route.element = <PrivateRoute>{route.element}</PrivateRoute>;
+    }
+    if (route?.children) {
+      route.children = authCheck(route.children);
+    }
+    return route;
+  });
+export default authCheck(routes);
